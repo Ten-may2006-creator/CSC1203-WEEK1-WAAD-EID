b51666a057d66252ef87ddf403294a6138a1219a
Ten-may2006-creator <Ten-may2006@outlook.com>
2026-01-30 14:28:04 +0300

    Week1 Submission

---
+++ lab_week1 A.py
@@ -0,0 +1,52 @@
+#Q-1#
+# in this line i want to print Welcome to Python Programming!
+print("Welcome to Python Programming!")
+#in this line i want to print my name and i add the methode \n to go to a newline(down)
+Name = "Waad"
+print("My name is:\n",Name) 
+#in this line i want to print the course name and i add the methode \n to go to a newline(down)
+Course_name = "Advanced_Programming_Methods"
+print("the course name is:\n" ,Course_name) 
+###########################
+#Q-2#
+#Numeric#
+age = 20
+gpa = 3.75
+print(gpa , type(gpa))
+print(age ,type(age))
+#########################
+#String#
+student_name = "Alice"
+print(student_name)
+print(type(student_name))
+##########################
+#list#
+subjects = ["Math","Python","Physics"]
+print(subjects)
+print(type(subjects))
+########################
+#dictionary#
+student_info ={
+    "id":101,
+    "year":2
+    }
+print(student_info)
+print(type(student_info))
+########################
+#set#
+skills = {
+    "Python",
+      "SQL"
+      }
+print(skills)
+print(type(skills))
+#######################
+#boolean#
+is_enrolled = True
+print(is_enrolled)
+print(type(is_enrolled))
+#######################
+#Nonetype#
+gradution_date = None
+print(gradution_date)
+print(type(gradution_date))
